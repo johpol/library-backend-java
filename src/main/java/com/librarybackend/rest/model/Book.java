3b4bd4cd2d35b152e5d06854098b666e0bcac31c
johpol <johpol@users.noreply.github.com>
2017-05-02 20:32:02 -0400

    book entity class: -annotated with JsonRootName -New field added with getter/setter

--- src/main/java/com/librarybackend/rest/model/Book.java
+++ src/main/java/com/librarybackend/rest/model/Book.java
@@ -1,5 +1,7 @@
 package com.librarybackend.rest.model;
 
+import com.fasterxml.jackson.annotation.JsonRootName;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,34 +11,47 @@ import javax.persistence.Id;
  * Created by John on 3/24/2017.
  */
 @Entity
+@JsonRootName(value = "book")
 public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    private Long _id;
     private String title;
     private String author;
     private String publisher;
-    private String subject;
     private String isbn;
+    private String tempIsbn;
+    private String subject;
 
     protected Book() {
     }
 
-    public Book(String title, String author, String publisher, String subject, String isbn) {
+    public Book(String _id, String title, String author, String publisher, String isbn, String tempIsbn, String subject) {
+        this._id = Long.valueOf(_id);
         this.title = title;
         this.author = author;
         this.publisher = publisher;
-        this.subject = subject;
         this.isbn = isbn;
+        this.tempIsbn = tempIsbn;
+        this.subject = subject;
     }
 
-    public Long getId() {
-        return id;
+    public Book(String _id, String title, String author, String publisher, String isbn, String subject) {
+        this._id = Long.valueOf(_id);
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.isbn = isbn;
+        this.subject = subject;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public Long get_id() {
+        return _id;
+    }
+
+    public void set_id(Long _id) {
+        this._id = _id;
     }
 
     public String getTitle() {
@@ -79,10 +94,18 @@ public class Book {
         this.isbn = isbn;
     }
 
+    public String getTempIsbn() {
+        return tempIsbn;
+    }
+
+    public void setTempIsbn(String tempIsbn) {
+        this.tempIsbn = tempIsbn;
+    }
+
     @Override
     public String toString() {
         return "Book{" +
-                "id=" + id +
+                "_id=" + _id +
                 ", title='" + title + '\'' +
                 ", author='" + author + '\'' +
                 ", publisher='" + publisher + '\'' +
